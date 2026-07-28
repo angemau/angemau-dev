1c91243fff9cc140a23420f37ee54f79a43ccd26
Angela <inge.angelalopez@gmail.com>
2026-07-28 17:28:48 -0500

    Add About Me section with content and styling; update i18n files for English and Spanish

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Hero } from './sections/hero/hero';
+import { AboutMe } from './sections/about-me/about-me';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero, AboutMe],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
