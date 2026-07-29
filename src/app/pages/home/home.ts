e737eb1c5babf53e941b1bcee95bd7c1b0240eb7
Angela <inge.angelalopez@gmail.com>
2026-07-29 12:16:29 -0500

    feat: add projects and skills sections with responsive design
    - Implemented projects section with dynamic content and styling.
    - Created skills section showcasing various technical skills.
    - Added CSS for both sections to ensure a cohesive design.
    - Utilized Angular's TranslatePipe for internationalization support.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,12 @@
 import { Hero } from './sections/hero/hero';
 import { AboutMe } from './sections/about-me/about-me';
+import { Skills } from './sections/skills/skills';
+import { Projects } from './sections/projects/projects';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, AboutMe],
+  imports: [Hero, AboutMe, Skills, Projects],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
