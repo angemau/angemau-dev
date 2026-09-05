e11054e50e23acda7b5a68d1af1cc1505def7e08
Angela <inge.angelalopez@gmail.com>
2026-09-04 21:19:20 -0500

    Adding contact and experience components with content and design

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,11 +2,13 @@ import { Hero } from './sections/hero/hero';
 import { AboutMe } from './sections/about-me/about-me';
 import { Skills } from './sections/skills/skills';
 import { Projects } from './sections/projects/projects';
+import { Experience } from './sections/experience/experience';
+import { Contact } from './sections/contact/contact';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, AboutMe, Skills, Projects],
+  imports: [Hero, AboutMe, Skills, Projects, Experience, Contact],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
